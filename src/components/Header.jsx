967ef2b39f5f5807e26e14d57f1c929c7cf01112
Yasmin Amorim Affonso <yasminamorimaffonso@gmail.com>
2025-09-22 20:23:35 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header({acao}) {
 
                 <div className="m-[10px] h-[125px] w-[250px] bg-[#CE6826] rounded-full flex justify-center items-center md:m-4 w-65 h-25 lg:w-95 h-30">
 
-                    <img src="/public/assets/imgs/Logo.svg" alt="" className="w-[150px] h-[150px] md:w-40 h-40 lg:w-[150px] h-[150px]" />
+                    <img src="/assets/imgs/Logo.svg" alt="" className="w-[150px] h-[150px] md:w-40 h-40 lg:w-[150px] h-[150px]" />
 
                 </div>
 
